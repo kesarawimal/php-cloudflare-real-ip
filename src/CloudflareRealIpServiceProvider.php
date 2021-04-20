2b13ccd17c826e9f3ac12625b709ddb93c6d1adb
Kesara Wimal <kesarawimal9@gmail.com>
2021-04-20 23:28:21 +0530

    ip range updated

--- src/CloudflareRealIpServiceProvider.php
+++ src/CloudflareRealIpServiceProvider.php
@@ -35,13 +35,30 @@ class CloudflareRealIpServiceProvider extends ServiceProvider
      * @return array
      * @var array
      */
-    protected static function ips()
-    {
-        $ipv4 = file_get_contents("../ips/ips-v4.txt");
-        $ipv6 = file_get_contents("../ips/ips-v6.txt");
-
-        return (array_merge(explode("\n", $ipv4), explode("\n", $ipv6))) ? array_filter(array_merge(explode("\n", $ipv4), explode("\n", $ipv6))) : [];
-    }
+    protected static $ips = [
+        '173.245.48.0/20',
+        '103.21.244.0/22',
+        '103.22.200.0/22',
+        '103.31.4.0/22',
+        '141.101.64.0/18',
+        '108.162.192.0/18',
+        '190.93.240.0/20',
+        '188.114.96.0/20',
+        '197.234.240.0/22',
+        '198.41.128.0/17',
+        '162.158.0.0/15',
+        '172.64.0.0/13',
+        '131.0.72.0/22',
+        '104.16.0.0/13',
+        '104.24.0.0/14',
+        '2400:cb00::/32',
+        '2606:4700::/32',
+        '2803:f800::/32',
+        '2405:b500::/32',
+        '2405:8100::/32',
+        '2a06:98c0::/29',
+        '2c0f:f248::/32'
+    ];
 
     /**
      * Checks if current request is coming from CloudFlare servers.
@@ -50,7 +67,7 @@ class CloudflareRealIpServiceProvider extends ServiceProvider
      */
     public static function isTrustedRequest()
     {
-        return IpUtils::checkIp(request()->ip(), self::ips());
+        return IpUtils::checkIp(request()->ip(), static::$ips);
     }
 
 
@@ -90,7 +107,7 @@ class CloudflareRealIpServiceProvider extends ServiceProvider
     public static function country()
     {
         return static::onTrustedRequest(function () {
-            return request()->header('HTTP_CF_IPCOUNTRY');
+            return request()->header('CF_IPCOUNTRY');
         }) ?: '';
     }
 
